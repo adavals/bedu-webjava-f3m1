d966f810b668d15bd636a34f63a84b43516141ab
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-05-28 18:44:45 -0600

    Update Etapa.java
    Agregando dependencias

--- src/main/java/org/bedu/java/backend/postwork/model/Etapa.java
+++ src/main/java/org/bedu/java/backend/postwork/model/Etapa.java
@@ -1,4 +1,14 @@
 package org.bedu.java.backend.postwork.model;
 
 public class Etapa {
+  
+  @PositiveOrZero(message = "El identificador  de la etapa no puede ser negativo.")
+    private long etapaId;
+  
+    @NotEmpty(message = "El nombre de la etapa no puede estar en blanco.")
+    @Size(min = 4, max = 30, message = "El nombre de la etapa debe tener entre 4 y 30 caracteres.")
+    private String nombre;
+  
+    @Positive(message = "La etapa debe tener un orden positivo mayor a cero.")
+    private int orden;
 }
