89acdf64c2394a5aa14d46b24b0fb1d73578a019
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-05-28 18:43:59 -0600

    Update Cliente.java
    Agregando dependencias

--- src/main/java/org/bedu/java/backend/postwork/model/Cliente.java
+++ src/main/java/org/bedu/java/backend/postwork/model/Cliente.java
@@ -1,4 +1,20 @@
 package org.bedu.java.backend.postwork.model;
 
 public class Cliente {
+  @PositiveOrZero(message = "El identificador no puede ser un numero negativo")
+    private long id;
+  
+    @NotEmpty(message = "El campo no puede estar vacio.")
+    @Size(min = 5, max = 30, message = "El nombre debe ser entre 5 y 30 letras.")
+    private String nombre;
+  
+    @Email
+    private String correoContacto;
+
+    @Min(value = 10, message = "Los clientes con menos de 10 empleados no son validos")
+    @Max(value = 10000, message = "Los clientes con mas de 10000 empleados no son validos")
+    private String numEmpleados;
+  
+    @NotBlank(message = "Se debe ingresar una direccion.")
+    private String direccion;
 }
