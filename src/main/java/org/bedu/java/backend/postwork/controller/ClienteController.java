56dd4f30578537739a201654daa620a283725a66
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2023-05-28 23:59:18 -0600

    Refactor: Limpieza de wildcards en importaciones

--- src/main/java/org/bedu/java/backend/postwork/controller/ClienteController.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/ClienteController.java
@@ -3,7 +3,14 @@ package org.bedu.java.backend.postwork.controller;
 import org.bedu.java.backend.postwork.model.Cliente;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.net.URI;
 import java.util.ArrayList;
