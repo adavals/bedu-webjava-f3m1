ac254fe7d7a05410e5ac3a79bfb2b551d7c58364
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-05-28 18:48:23 -0600

    Update Venta.java
    Agregando dependencias

--- src/main/java/org/bedu/java/backend/postwork/model/Venta.java
+++ src/main/java/org/bedu/java/backend/postwork/model/Venta.java
@@ -1,4 +1,19 @@
 package org.bedu.java.backend.postwork.model;
 
 public class Venta {
+  
+  @PositiveOrZero(message = "El identificador de la venta no puede ser negativo.")
+    private long ventaId;
+
+    @DecimalMin(value = "1.00", inclusive = true, message = "La venta debe ser de al menos 1.00")
+    private float monto;
+
+    @NotEmpty(message = "La venta debe tener por lo menos un producto.")
+    private List<Producto> productos;
+
+    @NotNull(message = "La venta debe haberse realizado a algún cliente.")
+    private Cliente cliente;
+
+    @PastOrPresent(message = "La venta no puede ocurrir en el futuro.")
+    private LocalDateTime fechaCreacion;
 }
