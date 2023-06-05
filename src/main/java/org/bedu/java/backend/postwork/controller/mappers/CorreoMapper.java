7ed600a42a4dc8a623c4e70e895a0456a9c21709
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:02:18 -0600

    Update CorreoMapper.java
    Updating CorreoMapper

--- src/main/java/org/bedu/java/backend/postwork/controller/mappers/CorreoMapper.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/mappers/CorreoMapper.java
@@ -1,7 +1,10 @@
 package org.bedu.java.backend.postwork.controller.mappers;
 
+import org.bedu.java.backend.postwork.persistence.entities.PlantillaCorreo;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CorreoMapper {
+    PlantillaCorreo correoCorreoModelToCorreoEntity(org.bedu.java.backend.postwork.model.PlantillaCorreo correoModel);
+    org.bedu.java.backend.postwork.model.Etapa correoEntityToCorreoModel(PlantillaCorreo correoModel);
 }
