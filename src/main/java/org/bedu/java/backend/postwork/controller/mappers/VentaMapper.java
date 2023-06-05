955e03116e640fbc8898579a4359f68144aec223
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:07:55 -0600

    Update VentaMapper.java
    Updating VentaMapper

--- src/main/java/org/bedu/java/backend/postwork/controller/mappers/VentaMapper.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/mappers/VentaMapper.java
@@ -1,7 +1,10 @@
 package org.bedu.java.backend.postwork.controller.mappers;
 
+import org.bedu.java.backend.postwork.persistence.entities.Venta;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface VentaMapper {
+  Venta ventaModelToVentaEntity(org.bedu.java.backend.postwork.model.Venta ventaModel);
+  org.bedu.java.backend.postwork.model.Venta ventaEntityToVentaModel(Venta venta);
 }
