bcc21c67e4c8ea3547c69730572c089819e08b00
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:03:33 -0600

    Update EtapaMapper.java
    Updating EtapaMapper

--- src/main/java/org/bedu/java/backend/postwork/controller/mappers/EtapaMapper.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/mappers/EtapaMapper.java
@@ -1,7 +1,10 @@
 package org.bedu.java.backend.postwork.controller.mappers;
 
+import org.bedu.java.backend.postwork.persistence.entities.Etapa;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface EtapaMapper {
+  Etapa etapaModelToEtapaEntity(org.bedu.java.backend.postwork.model.Etapa etapaModel);
+  org.bedu.java.backend.postwork.model.Etapa etapaEntityToEtapaModel(Etapa etapa);
 }
