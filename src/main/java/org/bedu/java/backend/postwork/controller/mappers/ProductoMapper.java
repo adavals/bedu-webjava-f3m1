20ab5c432e8a21e7f4dcc987ae2200eb95149920
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:06:52 -0600

    Update ProductoMapper.java
    Updating ProductoMapper

--- src/main/java/org/bedu/java/backend/postwork/controller/mappers/ProductoMapper.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/mappers/ProductoMapper.java
@@ -1,7 +1,10 @@
 package org.bedu.java.backend.postwork.controller.mappers;
 
+import org.bedu.java.backend.postwork.persistence.entities.Producto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ProductoMapper {
+  Producto productoModelToProductoEntity(org.bedu.java.backend.postwork.model.Producto productoModel);
+  org.bedu.java.backend.postwork.model.Producto productoEntityToProductoModel(Producto producto);
 }
