489f2d58df2166d04e3504400fe031a88009dc62
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:01:31 -0600

    Update ClienteMapper.java
    Updating ClienteMapper

--- src/main/java/org/bedu/java/backend/postwork/controller/mappers/ClienteMapper.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/mappers/ClienteMapper.java
@@ -1,7 +1,10 @@
 package org.bedu.java.backend.postwork.controller.mappers;
 
+import org.bedu.java.backend.postwork.persistence.entities.Cliente;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ClienteMapper {
+    Cliente clienteModelToClienteEntity(org.bedu.java.backend.postwork.model.Cliente clienteModel);
+    org.bedu.java.backend.postwork.model.Cliente clienteEntityToClienteModel(Cliente cliente);
 }
