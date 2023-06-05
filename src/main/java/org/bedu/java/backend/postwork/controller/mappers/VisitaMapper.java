19a6a7b1ede0ad7f65b497f7adf7bd144ae06371
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:08:46 -0600

    Update VisitaMapper.java
    Updating VisitaMapper

--- src/main/java/org/bedu/java/backend/postwork/controller/mappers/VisitaMapper.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/mappers/VisitaMapper.java
@@ -1,7 +1,10 @@
 package org.bedu.java.backend.postwork.controller.mappers;
 
+import org.bedu.java.backend.postwork.persistence.entities.Visita;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface VisitaMapper {
+  Visita visitaModelToVisitaEntity(org.bedu.java.backend.postwork.model.Visita visitaModel);
+  org.bedu.java.backend.postwork.model.Visita visitaEntityToVisitaModel(Visita visita);
 }
