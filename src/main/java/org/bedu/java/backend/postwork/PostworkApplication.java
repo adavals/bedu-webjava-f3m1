e9f9257a00ee981adf468af6966f9221e3efa01f
Pablos <119007541+Pablos0@users.noreply.github.com>
2023-06-04 20:11:40 -0600

    Update PostworkApplication.java
    Adding CommandLineRunner

--- src/main/java/org/bedu/java/backend/postwork/PostworkApplication.java
+++ src/main/java/org/bedu/java/backend/postwork/PostworkApplication.java
@@ -1,13 +1,19 @@
 package org.bedu.java.backend.postwork;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostworkApplication {
+public class PostworkApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostworkApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
