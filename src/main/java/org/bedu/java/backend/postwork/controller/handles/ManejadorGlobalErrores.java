2067b4cfb06d445b75c1668e435c6286214efe12
adavals <valenzuela.g@gmail.com>
2023-06-14 17:31:37 -0600

    exception handler add

--- src/main/java/org/bedu/java/backend/postwork/controller/handles/ManejadorGlobalErrores.java
+++ src/main/java/org/bedu/java/backend/postwork/controller/handles/ManejadorGlobalErrores.java
@@ -3,10 +3,12 @@ package org.bedu.java.backend.postwork.controller.handles;
 import org.bedu.java.backend.postwork.model.RespuestaError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class ManejadorGlobalErrores {
